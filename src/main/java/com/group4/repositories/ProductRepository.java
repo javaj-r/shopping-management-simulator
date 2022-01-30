48052c2a11e7b838cae181886e8c589a9abbaf2f
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-30 20:24:48 +0330

    Add ProductRepository. Closes #16.

---
+++ src/main/java/com/group4/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.group4.repositories;
+
+import com.group4.entities.Product;
+
+/**
+ * @author javid
+ * Created on 1/30/2022
+ */
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
