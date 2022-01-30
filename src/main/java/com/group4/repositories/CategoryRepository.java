d8b43e7bf04891edbb05623f5de43742d0cbbbf6
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-30 20:24:13 +0330

    Add CategoryRepository. Closes #14.

---
+++ src/main/java/com/group4/repositories/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.group4.repositories;
+
+import com.group4.entities.Category;
+
+import java.util.List;
+
+/**
+ * @author javid
+ * Created on 1/30/2022
+ */
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+    List<Category> findAllChildes(Category category);
+}
