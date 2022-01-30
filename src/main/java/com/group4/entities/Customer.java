d39f6d847af21252701b409a86f94b9e4beb4da0
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-30 20:10:58 +0330

    Add Customer. Closes #10.

---
+++ src/main/java/com/group4/entities/Customer.java
@@ -0,0 +1,54 @@
+package com.group4.entities;
+
+/**
+ * @author javid
+ * Created on 1/30/2022
+ */
+public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private Long nationalCode;
+    private String email;
+    private Long phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(Long nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
