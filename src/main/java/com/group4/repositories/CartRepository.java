0345823b744e680d6b80676f3c07e8d994e1d368
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-30 20:22:41 +0330

    Add CartRepository. Closes #13.

---
+++ src/main/java/com/group4/repositories/CartRepository.java
@@ -0,0 +1,10 @@
+package com.group4.repositories;
+
+import com.group4.entities.Cart;
+
+/**
+ * @author javid
+ * Created on 1/30/2022
+ */
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+}
