8aed2210e9f6463fbb4e4c83df8aa424140249bc
Javid Rafiee <javid.rafiee@gmail.com>
2022-01-31 18:22:30 +0330

    Add CartService. Closes #24.

---
+++ src/main/java/com/group4/services/CartService.java
@@ -0,0 +1,10 @@
+package com.group4.services;
+
+import com.group4.entities.Cart;
+
+/**
+ * @author javid
+ * Created on 1/31/2022
+ */
+public interface CartService extends Service<Cart, Integer> {
+}
