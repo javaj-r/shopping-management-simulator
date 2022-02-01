73a9736cded0cc8a35cd815b5312be7ea2482fec
Javid Rafiee <javid.rafiee@gmail.com>
2022-02-02 03:18:17 +0330

    Add CategoryServiceImpl. Closes #28.

---
+++ src/main/java/com/group4/services/implementation/CategoryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.group4.services.implementation;
+
+import com.group4.entities.Category;
+import com.group4.repositories.CategoryRepository;
+import com.group4.services.CategoryService;
+
+import java.util.List;
+
+/**
+ * @author javid
+ * Created on 1/31/2022
+ */
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository repository;
+
+    public CategoryServiceImpl(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<Category> findAllChildes(Category category) {
+        return repository.findAllChildes(category);
+    }
+
+    @Override
+    public Integer save(Category entity) {
+        return repository.save(entity);
+    }
+}
